5c2e8c5c3fa1712c0ddddc71c9b75182939e34bf
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-06 14:35:48 +0700

    refactor: only lint eslint for `ts`,`js` files
    drop `prettier`

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,9 @@
-'use strict';
-
 /**
  * @type {{ [key: string]: import("lint-staged").Commands | import("lint-staged").ConfigFn }}
  */
 const config = {
-  '*.ts': ['npx prettier --write', 'eslint --fix'],
-  '*.html': ['npx eslint --fix', 'prettier --write'],
-  '*.scss': 'npx prettier --write',
+  '*.ts': 'eslint --fix',
+  '*.js': 'eslint --fix',
   '*.json': 'npx prettier --write',
   '*.yml': 'npx prettier --write'
 };
